78e29626d1cb1d6c4d5774dc4b58d40db1890260
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-23 23:43:29 +0800

    Create authentication & authorization skeleton

---
+++ auth/__init__.py
@@ -0,0 +1,37 @@
+from flask import request
+from flask.json import jsonify
+from functools import wraps
+import jwt
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def get_token_auth_header():
+    pass
+
+
+def verify_decode_jwt(token):
+    pass
+
+
+def check_permissions(permission, payload):
+    pass
+
+
+def requires_auth(permission=""):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            payload = verify_decode_jwt(token)
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+
+    return requires_auth_decorator
