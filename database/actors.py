70a3c53e6418360be1d02af4c388ce6a620c99c8
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-24 22:02:21 +0800

    Add CRUD operations for Actors

--- database/actors.py
+++ database/actors.py
@@ -10,6 +10,30 @@ class Actors(db.Model):
     age = db.Column(db.String)
     gender = db.Column(db.String)
 
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+        }
+
     def __repr__(self):
         return "<Actors: {}, {}, {}, {}>".format(
             self.id, self.name, self.age, self.gender
