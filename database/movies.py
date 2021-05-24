bac1071cb1ab9149f2509675ea1be77c4eb57830
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-24 22:02:40 +0800

    Add CRUD operations for model Movies

--- database/movies.py
+++ database/movies.py
@@ -9,5 +9,27 @@ class Movies(db.Model):
     title = db.Column(db.String)
     release_date = db.Column(db.DateTime(120))
 
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+        }
+
     def __repr__(self):
         return "<Movies: {}, {}, {}>".format(self.id, self.title, self.release_date)
