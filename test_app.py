414d5ad5ccc4a01683d1c893dcfcf34cb4060277
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-24 21:58:21 +0800

    Mock a database for testing

--- test_app.py
+++ test_app.py
@@ -12,6 +12,44 @@ from database.actors import Actors
 token = "Bearer "
 role = ""
 roleList = ["casting_assistant", "casting_director", "executive_producer"]
+testdb_path = "postgres://postgres:postgres@localhost:5432/testdb"
+
+
+def mock_testdb():
+    sample_movies = [
+        {"title": "Dragon Age", "release_date": "01/01/2008"},
+        {"title": "Jurassic Park", "release_date": "01/01/2002"},
+        {"title": "Toys Story", "release_date": "01/01/2006"},
+        {"title": "Golden Flower", "release_date": "01/01/2015"},
+        {"title": "Sherlock Holme", "release_date": "01/01/2020"},
+        {"title": "Transformer", "release_date": "01/01/2018"},
+        {"title": "Inquisition", "release_date": "01/01/2007"},
+        {"title": "Inception", "release_date": "01/01/2015"},
+        {"title": "Matrix", "release_date": "01/01/2001"},
+        {"title": "Doraemon", "release_date": "01/01/1990"},
+        {"title": "How I met your mother", "release_date": "01/01/2012"},
+        {"title": "Big Bang Theory", "release_date": "01/01/2011"},
+    ]
+
+    sample_actors = [
+        {"name": "Tom Hanks", "age": 35, "gender": "male"},
+        {"name": "Robert De Niro", "age": 55, "gender": "male"},
+        {"name": "Katharine Hepburn", "age": 45, "gender": "female"},
+        {"name": "Humphrey Bogart", "age": 58, "gender": "female"},
+        {"name": "Meryl Streep", "age": 43, "gender": "male"},
+        {"name": "Kate Spade", "age": 39, "gender": "female"},
+        {"name": "Daniel Day-Lewis", "age": 76, "gender": "female"},
+        {"name": "Clark Gable", "age": 28, "gender": "female"},
+        {"name": "Sidney Poitier", "age": 19, "gender": "male"},
+        {"name": "Ingrid Bergman", "age": 69, "gender": "female"},
+        {"name": "Elizabeth Taylor", "age": 90, "gender": "male"},
+    ]
+
+    for movies in sample_movies:
+        Movies(movies["title"], movies["release_date"]).insert()
+
+    for actor in sample_actors:
+        Actors(actor["name"], actor["age"], actor["gender"]).insert()
 
 
 class MoviesTestCase(unittest.TestCase):
