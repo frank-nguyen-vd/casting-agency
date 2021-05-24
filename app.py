1c3d103dbab808e19dc44529104e00bdcced40e4
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-24 14:00:23 +0800

    Create Flask app only if required

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
-# THIRD-PARTY MODULES
 from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_cors import CORS
 
-# LOCAL MODULES
+
 from database import setup_db
 from database.movies import Movies
 from database.actors import Actors
 from auth import requires_auth
 
-load_dotenv()
-app = Flask(__name__)
-setup_db(app)
 
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-@app.route("/")
-def index():
-    return "Welcome to API"
+    @app.route("/")
+    def index():
+        return "Welcome to API"
 
+    @app.route("/movies")
+    @requires_auth("read:movies")
+    def get_movies(payload):
+        obj_list = Movies.query.all()
+        str_list = [str(obj) for obj in obj_list]
+        return jsonify({"success": True, "data": str_list})
 
-@app.route("/movies")
-@requires_auth("read:movies")
-def get_movies(payload):
-    obj_list = Movies.query.all()
-    str_list = [str(obj) for obj in obj_list]
-    return jsonify({"success": True, "data": str_list})
+    @app.route("/actors")
+    def get_actors():
+        obj_list = Actors.query.all()
+        str_list = [str(obj) for obj in obj_list]
+        return jsonify({"success": True, "data": str_list})
 
-
-@app.route("/actors")
-def get_actors():
-    obj_list = Actors.query.all()
-    str_list = [str(obj) for obj in obj_list]
-    return jsonify({"success": True, "data": str_list})
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run()
