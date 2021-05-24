e468ec20d1ad28c045fbc95e50b24a00ec2b9548
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-24 16:08:36 +0800

    Allow custom database_path upon intialization

--- database/__init__.py
+++ database/__init__.py
@@ -5,12 +5,15 @@ from flask_migrate import Migrate
 import json
 
 
-database_path = os.environ["DATABASE_URL"]
 db = SQLAlchemy()
 
 
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def setup_db(app, database_path=None):
+    if database_path is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     db.app = app
